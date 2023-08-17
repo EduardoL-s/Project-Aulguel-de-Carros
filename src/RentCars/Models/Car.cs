55ac73f9320cdb59e9e5bfc7e09b17dd974942e6
EduardoL-s <eduardoluizmm@hotmail.com>
2023-08-17 14:16:55 -0300

    [Requisito 6] Criação da class Car - Concluído

--- src/RentCars/Models/Car.cs
+++ src/RentCars/Models/Car.cs
@@ -2,6 +2,11 @@ using RentCars.Types;
 namespace RentCars.Models;
 
 //6 - Faça a classe `Car` herdar de `Vehicle`
-public class Car
+public class Car : Vehicle
 {
+    public int Seats {set; get;}
+    public int Doors {set; get;}
+    public TractionType Traction {set; get;}
+    public BrakeType FrontBrake {set; get;}
+    public BrakeType RearBrake {set; get;}
 }
