81206cab299e3dd21436371baba55ad2ca02b50a
EduardoL-s <eduardoluizmm@hotmail.com>
2023-08-17 14:19:21 -0300

    [Requisito 7] Criação da class Motorcycle herdanda da class Vehicle - Concluído

--- src/RentCars/Models/Motorcycle.cs
+++ src/RentCars/Models/Motorcycle.cs
@@ -5,4 +5,7 @@ namespace RentCars.Models;
 //7 - Faça a classe `Motorcycle` herdar de `Vehicle`
 public class Motorcycle : Vehicle
 {
+    public double SeatHeight {set;get;}
+    public BrakeType FrontBrake {set; get;}
+    public BrakeType RearBrake {set; get;}
 }
