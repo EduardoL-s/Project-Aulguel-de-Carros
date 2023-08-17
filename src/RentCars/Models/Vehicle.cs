cef7736709c358592df7e8a9ba1e4a04f86596f9
EduardoL-s <eduardoluizmm@hotmail.com>
2023-08-17 14:07:01 -0300

    [Requisito 3] Criação da Classe Vehicle - Concluído

--- src/RentCars/Models/Vehicle.cs
+++ src/RentCars/Models/Vehicle.cs
@@ -4,4 +4,13 @@ namespace RentCars.Models;
 //3 - Crie a classe `Vehicle`
 public class Vehicle
 {
+    public string Brand {set; get;} = "";
+    public string Model {set; get;} = "";
+    public decimal Price {set; get;}
+    public FuelType Fuel {set; get;}
+    public int EngineCapacity {set; get;}
+    public Color MainColor {set; get;}
+    public int Year {set; get;}
+    public double PricePerDay {set; get;}
+    public bool IsRented {set; get;} = false;
 }
